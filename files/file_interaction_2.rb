8efe1560ea9ddbd66a25b6f520a2d3cb729f7a3b
Ermias Giovanni <ermiasgiovanni@gmail.com>
2015-12-13 15:22:30 +0100

    Add exercises for 'Files' chapter

---
+++ files/file_interaction_2.rb
@@ -0,0 +1,15 @@
+require 'json'
+
+feedzilla_articles = []
+File.open("feedzilla.json", "r") do |f|
+  items = JSON.parse(f.read)
+  feedzilla_articles= items['articles'].map { |a|
+    {
+      title: a['title'],
+      link: a['url'],
+      summary: a['summary']
+    }
+  }
+end
+
+sorted_articles = (feedzilla_articles + slashdot_articles).sort_by {|a| a[:title]}
